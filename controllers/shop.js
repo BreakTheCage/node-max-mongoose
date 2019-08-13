f2099bfe9282cfeeda7368706b633a9755e80ebc
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-08-13 16:55:05 +0400

    #221; Deleteng cart Item

--- controllers/shop.js
+++ controllers/shop.js
@@ -72,7 +72,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCartPo(prodId)
     .then(result => {
       res.redirect("/cart");
     })
